071d5178899be815a03ce0151c1ab81240659d2d
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-11-22 15:24:01 -0600

    Couchbase Connect Utility

--- couchbase3/src/main/java/site/ycsb/db/couchbase3/CouchbaseConnect.java
+++ couchbase3/src/main/java/site/ycsb/db/couchbase3/CouchbaseConnect.java
@@ -4,10 +4,12 @@
 
 package site.ycsb.db.couchbase3;
 
+import com.couchbase.client.core.deps.io.netty.buffer.ByteBuf;
 import com.couchbase.client.core.env.IoConfig;
 import com.couchbase.client.core.env.NetworkResolution;
 import com.couchbase.client.core.env.TimeoutConfig;
 import com.couchbase.client.core.retry.BestEffortRetryStrategy;
+import com.couchbase.client.dcp.highlevel.internal.CollectionIdAndKey;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Scope;
 import com.couchbase.client.java.Cluster;
@@ -16,55 +18,83 @@ import com.couchbase.client.java.Collection;
 import com.couchbase.client.java.env.ClusterEnvironment;
 import com.couchbase.client.core.env.SecurityConfig;
 import com.couchbase.client.core.deps.io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import com.couchbase.client.dcp.Client;
+import com.couchbase.client.dcp.StreamFrom;
+import com.couchbase.client.dcp.StreamTo;
+import com.couchbase.client.dcp.message.DcpMutationMessage;
+import com.couchbase.client.dcp.message.MessageUtil;
 
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicLong;
 import java.time.Duration;
+import java.util.function.Consumer;
 
 /**
  * Couchbase Connection Utility.
  */
-@SuppressWarnings("InstantiationOfUtilityClass")
 public final class CouchbaseConnect {
   private static CouchbaseConnect instance;
-  private CouchbaseConnect() {
-  }
-  private static Cluster cluster;
-  private static final String DEFAULT_USER = "Administrator";
-  private static final String DEFAULT_PASSWORD = "password";
-  private static final String DEFAULT_HOSTNAME = "127.0.0.1";
+  private CouchbaseConnect() {}
+  private static Cluster pCluster;
+  private static Cluster xCluster;
+  private static Client pClient;
+  public static final String DEFAULT_USER = "Administrator";
+  public static final String DEFAULT_PASSWORD = "password";
+  public static final String DEFAULT_HOSTNAME = "127.0.0.1";
   private static final Boolean DEFAULT_SSL_MODE = false;
   private static final String DEFAULT_PROJECT = "project";
   private static final String DEFAULT_DATABASE = "database";
   private static final Boolean DEFAULT_EXTERNAL_MODE = false;
   private static final Object CONNECT_COORDINATOR = new Object();
+  private String primaryUser;
+  private String primaryPassword;
+  private String primaryConString;
+  private String primaryBucket;
+  private Boolean primarySslSetting;
+  private String xdcrUser;
+  private String xdcrPassword;
+  private String xdcrConString;
+  private String xdcrBucket;
+  private Boolean xdcrSslSetting;
 
-  public static synchronized CouchbaseConnect getInstance() {
+  /**
+   * Connect To Primary or XDCR.
+   */
+  public enum ConnectMode {
+    PRIMARY, XDCR
+  }
+
+  public static CouchbaseConnect getInstance() {
     if (instance == null) {
       instance = new CouchbaseConnect();
     }
     return instance;
   }
 
-  public static void connect() {
+  public void connect() {
     connectCluster(DEFAULT_HOSTNAME, DEFAULT_USER, DEFAULT_PASSWORD, DEFAULT_SSL_MODE,
-        DEFAULT_PROJECT, DEFAULT_DATABASE, DEFAULT_EXTERNAL_MODE);
+        DEFAULT_PROJECT, DEFAULT_DATABASE, DEFAULT_EXTERNAL_MODE, ConnectMode.PRIMARY);
   }
 
-  public static void connect(String hostname) {
-    connectCluster(hostname, DEFAULT_USER, DEFAULT_PASSWORD, DEFAULT_SSL_MODE, DEFAULT_PROJECT, DEFAULT_DATABASE, DEFAULT_EXTERNAL_MODE);
+  public void connect(String hostname) {
+    connectCluster(hostname, DEFAULT_USER, DEFAULT_PASSWORD, DEFAULT_SSL_MODE,
+        DEFAULT_PROJECT, DEFAULT_DATABASE, DEFAULT_EXTERNAL_MODE, ConnectMode.PRIMARY);
   }
 
-  public static void connect(String hostname, String username, String password) {
-    connectCluster(hostname, username, password, true, DEFAULT_PROJECT, DEFAULT_DATABASE, DEFAULT_EXTERNAL_MODE);
+  public void connect(String hostname, String username, String password, ConnectMode mode) {
+    connectCluster(hostname, username, password, true,
+        DEFAULT_PROJECT, DEFAULT_DATABASE, DEFAULT_EXTERNAL_MODE, mode);
   }
 
-  public static void connect(String hostname, String username, String password, String project, String database) {
-    connectCluster(hostname, username, password, true, project, database, DEFAULT_EXTERNAL_MODE);
+  public void connect(String hostname, String username, String password, String project, String database) {
+    connectCluster(hostname, username, password, true,
+        project, database, DEFAULT_EXTERNAL_MODE, ConnectMode.PRIMARY);
   }
 
-  public static void connectCluster(String hostname, String username, String password, Boolean ssl,
-                                    String project, String database, Boolean external) {
+  public void connectCluster(String hostname, String username, String password, Boolean ssl,
+                                    String project, String database, Boolean external, ConnectMode mode) {
     synchronized (CONNECT_COORDINATOR) {
-      if (cluster == null) {
+      if (pCluster == null) {
         StringBuilder connectBuilder = new StringBuilder();
         NetworkResolution network;
         String httpPrefix;
@@ -96,44 +126,162 @@ public final class CouchbaseConnect {
         connectBuilder.append(couchbasePrefix);
         connectBuilder.append(hostname);
 
-        String connectionString = connectBuilder.toString();
+        primaryConString = connectBuilder.toString();
 
-        ClusterEnvironment environment = ClusterEnvironment.builder()
-            .timeoutConfig(t -> TimeoutConfig
-                .kvTimeout(Duration.ofSeconds(5))
-                .connectTimeout(Duration.ofSeconds(20))
-                .queryTimeout(Duration.ofSeconds(75))
-            )
+        System.out.println(primaryConString);
+        System.out.println(ssl);
+
+        Consumer<SecurityConfig.Builder> secConfiguration = securityConfig -> {
+          securityConfig.enableTls(ssl)
+          .enableHostnameVerification(false)
+          .trustManagerFactory(InsecureTrustManagerFactory.INSTANCE);
+        };
+
+        Consumer<IoConfig.Builder> ioConfiguration = ioConfig -> {
+          ioConfig.enableTcpKeepAlives(true)
+          .tcpKeepAliveTime(Duration.ofSeconds(5))
+          .networkResolution(network);
+        };
+
+        Consumer<TimeoutConfig.Builder> timeOutConfiguration = timeoutConfig -> {
+          timeoutConfig.kvTimeout(Duration.ofSeconds(2))
+          .connectTimeout(Duration.ofSeconds(5))
+          .queryTimeout(Duration.ofSeconds(75));
+        };
+
+        ClusterEnvironment environment = ClusterEnvironment
+            .builder()
+            .timeoutConfig(timeOutConfiguration)
             .retryStrategy(BestEffortRetryStrategy.INSTANCE)
-            .ioConfig(i -> IoConfig.enableTcpKeepAlives(true)
-                .tcpKeepAliveTime(Duration.ofSeconds(5))
-                .networkResolution(network)
-            )
-            .securityConfig(s -> SecurityConfig.enableTls(ssl)
-                .enableHostnameVerification(false)
-                .trustManagerFactory(InsecureTrustManagerFactory.INSTANCE)
-            )
+            .ioConfig(ioConfiguration)
+            .securityConfig(secConfiguration)
             .build();
-        cluster = Cluster
-            .connect(connectionString,
+        System.out.println(environment.toString());
+        Cluster cluster = Cluster
+            .connect(primaryConString,
                 ClusterOptions.clusterOptions(username, password)
                     .environment(environment));
+
+        if (mode == ConnectMode.PRIMARY) {
+          pCluster = cluster;
+          primaryUser = username;
+          primaryPassword = password;
+          primarySslSetting = ssl;
+        } else {
+          xCluster = cluster;
+          xdcrUser = username;
+          xdcrPassword = password;
+          xdcrSslSetting = ssl;
+        }
       }
     }
   }
 
-  public static Collection keyspace(String bucket) {
-    return connectKeyspace(bucket, "_default", "_default");
+  public Client dcpConnect() {
+    try (Client client = Client.builder()
+        .connectionString(primaryConString)
+        .bucket(primaryBucket)
+        .credentials(primaryUser, primaryPassword)
+        .build()) {
+      return client;
+    }
   }
 
-  public static Collection keyspace(String bucket, String scope, String collection) {
-    return connectKeyspace(bucket, scope, collection);
+  public void getDocs(String bucket, ConnectMode mode) {
+    String conString;
+    String username;
+    String password;
+
+    Consumer<com.couchbase.client.dcp.SecurityConfig.Builder> secClientConfig = securityConfig -> {
+      securityConfig.enableTls(primarySslSetting)
+          .enableHostnameVerification(false)
+          .trustManagerFactory(InsecureTrustManagerFactory.INSTANCE);
+    };
+
+    if (mode == ConnectMode.PRIMARY) {
+      conString = primaryConString;
+      username = primaryUser;
+      password = primaryPassword;
+    } else {
+      conString = xdcrConString;
+      username = xdcrUser;
+      password = xdcrPassword;
+    }
+
+    Client client = Client.builder()
+        .connectionString(conString)
+        .bucket(bucket)
+        .securityConfig(secClientConfig)
+        .credentials(username, password)
+        .build();
+
+    client.controlEventHandler((flowController, event) -> {
+      flowController.ack(event);
+      event.release();
+    });
+
+    // Collect the statistics on mutations
+    final AtomicLong totalSize = new AtomicLong(0);
+    final AtomicLong docCount = new AtomicLong(0);
+
+    client.dataEventHandler((flowController, event) -> {
+      if (DcpMutationMessage.is(event)) {
+        CollectionIdAndKey key = MessageUtil.getCollectionIdAndKey(event, false);
+        ByteBuf content = DcpMutationMessage.content(event);
+        System.out.println("Key     : " + key.key());
+        System.out.println("Mutation: " + DcpMutationMessage.toString(event));
+        System.out.println("Document: " + DcpMutationMessage.content(event).toString(StandardCharsets.UTF_8));
+        docCount.incrementAndGet();
+        totalSize.addAndGet(DcpMutationMessage.content(event).readableBytes());
+      }
+      event.release();
+    });
+
+    // Connect the sockets
+    client.connect().block();
+
+    // Initialize the state (start now, never stop)
+    client.initializeState(StreamFrom.BEGINNING, StreamTo.NOW).block();
+
+    // Start streaming on all partitions
+    client.startStreaming().block();
+
+    // Sleep and wait until the DCP stream has caught up with the time where we said "now".
+    while (!client.sessionState().isAtEnd()) {
+      try {
+        Thread.sleep(500L);
+      } catch (InterruptedException ex) {
+        Thread.currentThread().interrupt();
+      }
+    }
+
+    System.out.println("Total Docs: " + docCount.get());
+    System.out.println("Total Bytes: " + totalSize.get());
+    System.out.println("Average Size per Doc: " + totalSize.get() / docCount.get() + "b");
+
+    // Proper Shutdown
+    client.disconnect().block();
   }
 
-  public static Collection connectKeyspace(String bucket, String scope, String collection) {
+  public Collection keyspace(String bucket, ConnectMode mode) {
+    return connectKeyspace(bucket, "_default", "_default", mode);
+  }
+
+  public Collection keyspace(String bucket, String scope, String collection, ConnectMode mode) {
+    return connectKeyspace(bucket, scope, collection, mode);
+  }
+
+  public Collection connectKeyspace(String bucket, String scope, String collection, ConnectMode mode) {
+    Cluster cluster;
+    if (mode == ConnectMode.PRIMARY) {
+      cluster = pCluster;
+    } else {
+      cluster = xCluster;
+    }
     Bucket bucketObj = cluster.bucket(bucket);
     bucketObj.waitUntilReady(Duration.ofSeconds(10));
     Scope scopeObj = bucketObj.scope(scope);
+    primaryBucket = bucket;
     return scopeObj.collection(collection);
   }
 }
--- couchbase3/src/main/java/site/ycsb/db/couchbase3/TSCompare.java
+++ couchbase3/src/main/java/site/ycsb/db/couchbase3/TSCompare.java
@@ -1,22 +1,50 @@
 package site.ycsb.db.couchbase3;
 
+import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.kv.GetResult;
 import org.apache.commons.cli.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 /**
  * Compare Timestamps.
  */
 public final class TSCompare {
+  public static final String SOURCE_HOST = "source.hostname";
+  public static final String SOURCE_USER = "source.user";
+  public static final String SOURCE_PASSWORD = "source.password";
+  public static final String SOURCE_BUCKET = "source.bucket";
+
+  private static void checkTimestamps(Properties properties) throws RuntimeException {
+    CouchbaseConnect db;
+    String sourceHost = properties.getProperty(SOURCE_HOST, CouchbaseConnect.DEFAULT_HOSTNAME);
+    String sourceUser = properties.getProperty(SOURCE_USER, CouchbaseConnect.DEFAULT_USER);
+    String sourcePassword = properties.getProperty(SOURCE_PASSWORD, CouchbaseConnect.DEFAULT_PASSWORD);
+    String sourceBucket = properties.getProperty(SOURCE_BUCKET, "ycsb");
+
+    System.out.println("Host: " + sourceHost);
+    System.out.println("User: " + sourceUser);
+
+    db = CouchbaseConnect.getInstance();
+
+    db.connect(sourceHost, sourceUser, sourcePassword, CouchbaseConnect.ConnectMode.PRIMARY);
+
+    Collection collection = db.keyspace(sourceBucket, CouchbaseConnect.ConnectMode.PRIMARY);
+    GetResult result = collection.get("usertable::user1000053778378872380");
+    System.out.println(result.toString());
+    db.getDocs(sourceBucket, CouchbaseConnect.ConnectMode.PRIMARY);
+  }
+
   public static void main(String[] args) {
     Options options = new Options();
-    CommandLine cmd;
+    CommandLine cmd = null;
+    Properties properties = new Properties();
 
-    Option source = new Option("s", "source", true, "source properties");
+    Option source = new Option("p", "properties", true, "source properties");
     source.setRequired(true);
     options.addOption(source);
-
-    Option target = new Option("t", "target", true, "target properties");
-    target.setRequired(true);
-    options.addOption(target);
 
     CommandLineParser parser = new DefaultParser();
     HelpFormatter formatter = new HelpFormatter();
@@ -25,8 +53,25 @@ public final class TSCompare {
       cmd = parser.parse(options, args);
     } catch (ParseException e) {
       System.out.println(e.getMessage());
-      formatter.printHelp("utility-name", options);
+      formatter.printHelp("TSCompare", options);
 
+      System.exit(1);
+    }
+
+    String propFile = cmd.getOptionValue("properties");
+
+    try {
+      properties.load(Files.newInputStream(Paths.get(propFile)));
+    } catch (IOException e) {
+      System.out.println("can not open properties file: " + e.getMessage());
+      System.exit(1);
+    }
+
+    try {
+      System.out.println("Starting");
+      checkTimestamps(properties);
+    } catch (RuntimeException e) {
+      System.err.println("Error: " + e);
       System.exit(1);
     }
   }
