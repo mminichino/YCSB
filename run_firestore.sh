d15bc345f18432c2da5f8360b989cb114a59c852
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-02-23 21:48:38 -0600

    Version 2.0.0

--- run_firestore.sh
+++ run_firestore.sh
@@ -3,12 +3,14 @@
 SCRIPT_PATH=$(dirname "$0")
 SCRIPT_ROOT=$(cd "$SCRIPT_PATH/.." && pwd)
 CLASSPATH="${SCRIPT_ROOT}/conf:${SCRIPT_ROOT}/lib/*:${SCRIPT_ROOT}/googlefirestore-binding/lib/*"
+DATABASE="testdb"
+REGION="us-central1"
 THREADCOUNT_LOAD=32
 THREADCOUNT_RUN=256
 RECORDCOUNT=1000000
 OPCOUNT=10000000
 RUNTIME=180
-PRINT_USAGE="Usage: $0 [ -T run_time -O operations -R record_count ]"
+PRINT_USAGE="Usage: $0 [ -d database -r region -T run_time -O operations -R record_count ]"
 
 function print_usage {
 if [ -n "$PRINT_USAGE" ]; then
@@ -16,9 +18,15 @@ if [ -n "$PRINT_USAGE" ]; then
 fi
 }
 
-while getopts "R:O:T:" opt
+while getopts "d:r:R:O:T:" opt
 do
   case $opt in
+    d)
+      DATABASE=$OPTARG
+      ;;
+    r)
+      REGION=$OPTARG
+      ;;
     R)
       RECORDCOUNT=$OPTARG
       ;;
@@ -42,6 +50,10 @@ do
   LOAD_OPTS="-db site.ycsb.db.firestore.GoogleFirestoreClient -P conf/googlefirestore.properties -P $workload -threads $THREADCOUNT_LOAD -p recordcount=$RECORDCOUNT -p core_workload_insertion_retry_limit=10 -s -load"
   RUN_OPTS="-db site.ycsb.db.firestore.GoogleFirestoreClient -P conf/googlefirestore.properties -P $workload -threads $THREADCOUNT_RUN -p recordcount=$RECORDCOUNT -p operationcount=$OPCOUNT -p maxexecutiontime=$RUNTIME -s -t"
 
+  echo "Creating Firestore database ${DATABASE}"
+  gcloud firestore databases create --location="${REGION}" --database="${DATABASE}"
   java -cp "$CLASSPATH" site.ycsb.Client $LOAD_OPTS
   java -cp "$CLASSPATH" site.ycsb.Client $RUN_OPTS
+  echo "Deleting Firestore database ${DATABASE}"
+  gcloud firestore databases delete --database="${DATABASE}" -q
 done
