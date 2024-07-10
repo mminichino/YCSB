8aa4a4cd96265f963799222434e26f09c93f7b07
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-07-10 10:04:34 -0500

    Version 2.0.0

--- snowflake/src/main/java/site/ycsb/db/snowflake/CH2Queries.java
+++ snowflake/src/main/java/site/ycsb/db/snowflake/CH2Queries.java
@@ -151,7 +151,7 @@ public class CH2Queries extends BenchQueries {
           "AND  c.c_d_id = o.o_d_id " +
           "AND  o.o_entry_d >= '2015-10-01 00:00:00.000000' " +
           "AND o.o_entry_d < '2016-01-01 00:00:00.000000' " +
-          "AND  n.n_nationkey = string_to_codepoint(c.c_state)[0] " +
+          "AND  n.n_nationkey = UNICODE(c.c_state) " +
           "GROUP BY c.c_id, c.c_last, c.c_city, c.c_phone, n.n_name " +
           "ORDER BY revenue DESC " +
           "LIMIT 20",
