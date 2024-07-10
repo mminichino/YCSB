5c6f7b4dd370ef0ded48e54c6fbe9e2efb8d386a
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-07-10 10:01:04 -0500

    Version 2.0.0

--- snowflake/src/main/java/site/ycsb/db/snowflake/CH2Queries.java
+++ snowflake/src/main/java/site/ycsb/db/snowflake/CH2Queries.java
@@ -100,31 +100,31 @@ public class CH2Queries extends BenchQueries {
           "GROUP BY su.su_nationkey, SUBSTR(n1n2cools.c_state,1,1), DATE_PART(year, n1n2cools.o_entry_d) " +
           "ORDER BY su.su_nationkey, cust_nation, l_year",
       // Q08
-      "SELECT DATE_PART_STR(rn1coolis.o_entry_d, 'year') as l_year, " +
+      "SELECT DATE_PART(year, rn1coolis.o_entry_d) as l_year, " +
           "ROUND((SUM(case when sun2.n_name = 'Germany' then rn1coolis.ol_amount else 0 end) / SUM(rn1coolis.ol_amount)),2) as mkt_share " +
           "FROM " +
           "(SELECT rn1cooli.o_entry_d,  rn1cooli.ol_amount, s.s_w_id, s.s_i_id " +
           "FROM stock s JOIN " +
           "(SELECT o.o_entry_d, ol.ol_i_id, ol.ol_amount, ol.ol_supply_w_id " +
-          "FROM orders o, order_line ol, item i JOIN " +
+          "FROM orders o JOIN order_line ol ON o.o_id = ol.ol_o_id JOIN item i JOIN " +
           "(SELECT c.c_id, c.c_w_id, c.c_d_id " +
           "FROM customer c JOIN " +
           "(SELECT n1.n_nationkey " +
           "FROM nation n1, region r " +
           "WHERE n1.n_regionkey = r.r_regionkey AND r.r_name = 'Europe') nr " +
-          "ON nr.n_nationkey = string_to_codepoint(c.c_state)[0]) cnr " +
+          "ON nr.n_nationkey = UNICODE(c.c_state)) cnr " +
           "ON cnr.c_id = o.o_c_id AND cnr.c_w_id = o.o_w_id AND cnr.c_d_id = o.o_d_id " +
           "AND i.i_data LIKE '%b' AND i.i_id = ol.ol_i_id " +
           "AND ol.ol_i_id < 1000 " +
-          "AND o.o_entry_d /*+ skip-index */ BETWEEN '2017-01-01 00:00:00.000000' AND '2018-12-31 00:00:00.000000' " +
-          "WHERE o.o_id = ol.ol_o_id) rn1cooli " +
+          "AND o.o_entry_d BETWEEN '2017-01-01 00:00:00.000000' AND '2018-12-31 00:00:00.000000' " +
+          ") rn1cooli " +//
           "ON rn1cooli.ol_i_id = s.s_i_id " +
           "AND rn1cooli.ol_supply_w_id = s.s_w_id) rn1coolis JOIN " +
           "(SELECT su.su_suppkey, n2.n_name " +
           "FROM supplier su, nation n2 " +
           "WHERE su.su_nationkey = n2.n_nationkey) sun2 " +
           "ON MOD(rn1coolis.s_w_id * rn1coolis.s_i_id, 10000) = sun2.su_suppkey " +
-          "GROUP BY DATE_PART_STR(rn1coolis.o_entry_d, 'year') " +
+          "GROUP BY DATE_PART(year, rn1coolis.o_entry_d) " +
           "ORDER BY l_year",
       // Q09
       "SELECT sun.n_name, DATE_PART_STR(oolis.o_entry_d, 'year') as l_year, round (SUM(oolis.ol_amount), 2) as SUM_profit " +
