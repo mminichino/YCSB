9b8d02e3605bce95c1b547bd2d6e90df53380479
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-07-10 10:18:38 -0500

    Version 2.0.0

--- snowflake/src/main/java/site/ycsb/db/snowflake/CH2Queries.java
+++ snowflake/src/main/java/site/ycsb/db/snowflake/CH2Queries.java
@@ -163,11 +163,11 @@ public class CH2Queries extends BenchQueries {
           "AND  n.n_name = 'Germany' " +
           "GROUP BY s.s_i_id " +
           "HAVING SUM(s.s_order_cnt) > " +
-          "(SELECT VALUE SUM(s1.s_order_cnt) * 0.00005 " +
+          "(SELECT SUM(s1.s_order_cnt) * 0.00005 " +
           "FROM nation n1, supplier su1, stock s1 " +
           "WHERE MOD(s1.s_w_id * s1.s_i_id, 10000) = su1.su_suppkey " +
           "AND su1.su_nationkey = n1.n_nationkey " +
-          "AND n1.n_name = 'Germany')[0] " +
+          "AND n1.n_name = 'Germany') " +
           "ORDER BY ordercount DESC",
       // Q12
       "SELECT o.o_ol_cnt, " +
